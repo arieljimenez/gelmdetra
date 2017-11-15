096efcf847454aaf0e6426abdc3af15712e52b4c
Ariel Jimenez <frismaury@gmail.com>
2017-11-15 00:25:22 -0400

    add: dev: Add docker-compose

--- init.sh
+++ init.sh
@@ -1,16 +1,20 @@
-#!/bin/sh
-ctx=`docker ps -aqf name=gedetrax`
-if [ -z $ctx ]; then
-  echo "================================"
-  echo "[Creating container for DEV ENV]"
+#!/bin/bash
 
-  docker run -it \
-    --name gedetrax \
-    -p 8000:8000 -p 8080:8080 \
-    -v ${PWD}:/app \
-    frismaury/gelmdetra:v1
-else
-  echo "============================"
-  echo "[Running gELMdetra container]"
-  docker start -i gedetrax
-fi
+docker-compose up --remove-orphans
+
+# DEPRECATED
+# ctx=`docker ps -aqf name=gedetrax`
+# if [ -z $ctx ]; then
+#   echo "================================"
+#   echo "[Creating container for DEV ENV]"
+
+#   docker run -it \
+#     --name gedetrax \
+#     -p 8000:8000 -p 8080:8080 \
+#     -v ${PWD}:/app \
+#     frismaury/gelmdetra:v1
+# else
+#   echo "============================"
+#   echo "[Running gELMdetra container]"
+#   docker start -i gedetrax
+# fi
